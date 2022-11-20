daea0c8ce9bd309903b19cf1ee359d350ea24fa1
Denito37 <69406178+Denito37@users.noreply.github.com>
2022-11-20 06:53:59 -0500

    Gave functionality to tip btns

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //Variables
-const btns = document.querySelector(".tipbtn")[0];//first btn
-const btns2 = document.querySelector(".tipbtn")[1];//second btn
-const btns3 = document.querySelector(".tipbtn")[2];//third btn
-const btns4 = document.querySelector(".tipbtn")[3];//fourth btn
-const btns5 = document.querySelector(".tipbtn")[4];//fifth btn
+const btn = document.querySelector("#btn5");//first btn
+const btn2 = document.querySelector("#btn10");//second btn
+const btn3 = document.querySelector("#btn15");//third btn
+const btn4 = document.querySelector("#btn25");//fourth btn
+const btn5 = document.querySelector("#btn50");//fifth btn
 const custom = document.getElementById('custom');//input
 const errMsg = document.getElementById('err1');
 const errMsgTwo = document.getElementById('err2');
@@ -12,12 +12,33 @@ const ppl = document.getElementById('ppl'); //input
 const tip = document.getElementById('just-tip');//result 1
 const total = document.getElementById('total'); // result 2
 const reset = document.getElementById('reset');
+let tipValue = 0;
 //Events
 bill.addEventListener('input', isZero);
 ppl.addEventListener('input', isZero);
 bill.addEventListener('change', totalCalc);
 ppl.addEventListener('change', totalCalc);
 reset.addEventListener('click', resetValues);
+btn.addEventListener('click', () =>{
+   btn.classList.toggle('changeClr')
+   tipValue = 0.05;
+})
+btn2.addEventListener('click',() =>{
+   btn2.classList.toggle('changeClr')
+   tipValue = 0.10;
+})
+btn3.addEventListener('click',() =>{
+   btn3.classList.toggle('changeClr')
+   tipValue = 0.15;
+})
+btn4.addEventListener('click',() =>{
+   btn4.classList.toggle('changeClr')
+   tipValue = 0.25;
+})
+btn5.addEventListener('click',() =>{
+   btn5.classList.toggle('changeClr')
+   tipValue = 0.50;
+})
 //Functions
 function isZero(){
    if(bill.value === '0' ){
@@ -37,11 +58,13 @@ function totalCalc(){
    let cash = bill.value; 
    let person = ppl.value; 
    let totalPay = (cash/person);
-   let tipCharge = ((custom.value)/100);
+   let tipCharge = ((custom.value)/100) || tipValue;
    let tipPP = totalPay * tipCharge
-   let TotalPP = parseFloat(tipPP) + parseFloat(totalPay)
+   let totalPP = parseFloat(tipPP) + parseFloat(totalPay)
    tip.innerHTML = "$" + tipPP.toFixed(2);
-   total.innerHTML = "$" + TotalPP.toFixed(2); 
+   total.innerHTML = "$" + totalPP.toFixed(2); 
+   if(isNaN(totalPP) || totalPP == "Infinity"){ total.innerHTML ="$0.00";}
+   if(isNaN(tipPP) || tipPP == "Infinity"){ tip.innerHTML ="$0.00";}
 }
 function resetValues(){
    bill.value = "";
